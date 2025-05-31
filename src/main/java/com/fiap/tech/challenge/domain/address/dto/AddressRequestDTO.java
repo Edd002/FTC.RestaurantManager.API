a3d95c769d283064b1d24d62a41c529bff88e344
Eduardo Augusto Lima Pereira <42621512+Edd002@users.noreply.github.com>
2025-05-31 17:04:46 -0300

    ##Fase-1-testes-automatizados-user : Testes automatizados de criação de usuário

--- src/main/java/com/fiap/tech/challenge/domain/address/dto/AddressRequestDTO.java
+++ src/main/java/com/fiap/tech/challenge/domain/address/dto/AddressRequestDTO.java
@@ -3,7 +3,7 @@ package com.fiap.tech.challenge.domain.address.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fiap.tech.challenge.global.base.dto.BaseRequestDTO;
-import com.fiap.tech.challenge.global.util.deserializer.StrictStringDeserializer;
+import com.fiap.tech.challenge.global.util.deserializer.StrictStringNormalizeSpaceDeserializer;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -15,48 +15,48 @@ public abstract class AddressRequestDTO extends BaseRequestDTO {
     @Schema(description = "Descrição do endereço.", example = "Rua dos Cadomblés", maxLength = 255)
     @Size(max = 255, message = "O número de caracteres máximo para a descrição do endereço é 255 caracteres.")
     @NotBlank(message = "A descrição do endereço não pode ser nula ou em branco.")
-    @JsonDeserialize(using = StrictStringDeserializer.class)
+    @JsonDeserialize(using = StrictStringNormalizeSpaceDeserializer.class)
     @JsonProperty("description")
     private String description;
 
     @Schema(description = "Número do endereço.", example = "34", maxLength = 255)
     @Size(max = 255, message = "O número de caracteres máximo para o número do endereço é 255 caracteres.")
     @NotBlank(message = "O número do endereço não pode ser nulo ou em branco.")
-    @JsonDeserialize(using = StrictStringDeserializer.class)
+    @JsonDeserialize(using = StrictStringNormalizeSpaceDeserializer.class)
     @JsonProperty("number")
     private String number;
 
     @Schema(description = "Complemento do endereço.", example = "Casa A", maxLength = 255)
     @Size(max = 255, message = "O número de caracteres máximo para o complemento do endereço é 255 caracteres.")
-    @JsonDeserialize(using = StrictStringDeserializer.class)
+    @JsonDeserialize(using = StrictStringNormalizeSpaceDeserializer.class)
     @JsonProperty("complement")
     private String complement;
 
     @Schema(description = "Bairro do endereço.", example = "Santo Antônio", maxLength = 255)
     @Size(max = 255, message = "O número de caracteres máximo para o bairro do endereço é 255 caracteres.")
     @NotBlank(message = "O bairro do endereço não pode ser nulo ou em branco.")
-    @JsonDeserialize(using = StrictStringDeserializer.class)
+    @JsonDeserialize(using = StrictStringNormalizeSpaceDeserializer.class)
     @JsonProperty("neighborhood")
     private String neighborhood;
 
     @Schema(description = "CEP do endereço.", example = "35090-650", maxLength = 255)
     @Size(max = 255, message = "O número de caracteres máximo para o CEP do endereço é 255 caracteres.")
     @NotBlank(message = "O CEP do endereço não pode ser nulo ou em branco.")
-    @JsonDeserialize(using = StrictStringDeserializer.class)
+    @JsonDeserialize(using = StrictStringNormalizeSpaceDeserializer.class)
     @JsonProperty("cep")
     private String cep;
 
     @Schema(description = "Caixa postal do endereço.", example = "1234-5678", maxLength = 255)
     @Size(max = 255, message = "O número de caracteres máximo para o código postal do endereço é 255 caracteres.")
     @NotBlank(message = "A caixa postal do endereço não pode ser nula ou em branco.")
-    @JsonDeserialize(using = StrictStringDeserializer.class)
+    @JsonDeserialize(using = StrictStringNormalizeSpaceDeserializer.class)
     @JsonProperty("postalCode")
     private String postalCode;
 
     @Schema(description = "Hash id da cidade do endereço.", example = "7edacee2252544519c29240daa51ee97", maxLength = 255)
     @Size(max = 255, message = "O número de caracteres máximo para o hash id da cidade do endereço é 255 caracteres.")
     @NotBlank(message = "A hash id da cidade do endereço não pode ser nulo ou em branco.")
-    @JsonDeserialize(using = StrictStringDeserializer.class)
+    @JsonDeserialize(using = StrictStringNormalizeSpaceDeserializer.class)
     @JsonProperty("hashIdCity")
     private String hashIdCity;
 }
--- src/test/java/com/fiap/tech/challenge/domain/user/UserControllerTest.java
+++ src/test/java/com/fiap/tech/challenge/domain/user/UserControllerTest.java
@@ -1,10 +1,115 @@
 package com.fiap.tech.challenge.domain.user;
 
+import com.fiap.tech.challenge.domain.user.dto.UserPostRequestDTO;
+import com.fiap.tech.challenge.domain.user.dto.UserResponseDTO;
+import com.fiap.tech.challenge.global.base.response.error.BaseErrorResponse403;
+import com.fiap.tech.challenge.global.base.response.success.BaseSuccessResponse201;
+import com.fiap.tech.challenge.global.component.DatabaseManagementComponent;
+import com.fiap.tech.challenge.global.component.HttpBodyComponent;
+import com.fiap.tech.challenge.global.component.HttpHeaderComponent;
+import com.fiap.tech.challenge.global.util.JsonUtil;
+import com.fiap.tech.challenge.global.util.ValidationUtil;
+import com.fiap.tech.challenge.global.util.enumerated.DatePatternEnum;
+import com.google.gson.reflect.TypeToken;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
 
 @ExtendWith(value = {SpringExtension.class})
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserControllerTest {
+
+    private final HttpHeaderComponent httpHeaderComponent;
+    private final HttpBodyComponent httpBodyComponent;
+    private final TestRestTemplate testRestTemplate;
+    private final DatabaseManagementComponent databaseManagementComponent;
+
+    @Autowired
+    public UserControllerTest(HttpHeaderComponent httpHeaderComponent, HttpBodyComponent httpBodyComponent, TestRestTemplate testRestTemplate, DatabaseManagementComponent databaseManagementComponent) {
+        this.httpHeaderComponent = httpHeaderComponent;
+        this.httpBodyComponent = httpBodyComponent;
+        this.testRestTemplate = testRestTemplate;
+        this.databaseManagementComponent = databaseManagementComponent;
+    }
+
+    private static final String PATH_RESOURCE_USER = "/mock/user/user.json";
+
+    @BeforeEach
+    public void populateDatabase() {
+        List<String> sqlFileScripts = List.of(
+                "persistence/state/before_test_state.sql",
+                "persistence/city/before_test_city.sql",
+                "persistence/address/before_test_address.sql",
+                "persistence/loadtable/before_test_load_table.sql",
+                "persistence/user/before_test_user.sql",
+                "persistence/jwt/before_test_jwt.sql"
+        );
+        databaseManagementComponent.populateDatabase(sqlFileScripts);
+    }
+
+    @AfterEach
+    public void clearDatabase() {
+        databaseManagementComponent.clearDatabase();
+    }
+
+    @DisplayName(value = "Teste de sucesso - Criar um usuário dono")
+    @Test
+    public void createUserOwnerSuccess() {
+        HttpHeaders headers = httpHeaderComponent.generateHeaderWithOwnerBearerToken();
+        UserPostRequestDTO userPostRequestDTO = JsonUtil.objectFromJson("userPostRequestDTOOwner", PATH_RESOURCE_USER, UserPostRequestDTO.class, DatePatternEnum.DATE_FORMAT_mm_dd_yyyy_WITH_SLASH.getValue());
+        ResponseEntity<?> responseEntity = testRestTemplate.exchange("/api/v1/users", HttpMethod.POST, new HttpEntity<>(userPostRequestDTO, headers), new ParameterizedTypeReference<>() {});
+        BaseSuccessResponse201<UserResponseDTO> responseObject = httpBodyComponent.responseEntityToObject(responseEntity, new TypeToken<>() {});
+        Assertions.assertEquals(HttpStatus.CREATED.value(), responseEntity.getStatusCode().value());
+        Assertions.assertEquals(HttpStatus.CREATED.value(), responseObject.getStatus());
+        Assertions.assertTrue(responseObject.isSuccess());
+        Assertions.assertTrue(ValidationUtil.isNotBlank(responseObject.getItem().getHashId()));
+        Assertions.assertTrue(ValidationUtil.isNotBlank(responseObject.getItem().getAddress().getHashId()));
+    }
+
+    @DisplayName(value = "Teste de sucesso - Criar um usuário cliente")
+    @Test
+    public void createUserClientSuccess() {
+        HttpHeaders headers = httpHeaderComponent.generateHeaderWithOwnerBearerToken();
+        UserPostRequestDTO userPostRequestDTO = JsonUtil.objectFromJson("userPostRequestDTOClient", PATH_RESOURCE_USER, UserPostRequestDTO.class, DatePatternEnum.DATE_FORMAT_mm_dd_yyyy_WITH_SLASH.getValue());
+        ResponseEntity<?> responseEntity = testRestTemplate.exchange("/api/v1/users", HttpMethod.POST, new HttpEntity<>(userPostRequestDTO, headers), new ParameterizedTypeReference<>() {});
+        BaseSuccessResponse201<UserResponseDTO> responseObject = httpBodyComponent.responseEntityToObject(responseEntity, new TypeToken<>() {});
+        Assertions.assertEquals(HttpStatus.CREATED.value(), responseEntity.getStatusCode().value());
+        Assertions.assertEquals(HttpStatus.CREATED.value(), responseObject.getStatus());
+        Assertions.assertTrue(responseObject.isSuccess());
+        Assertions.assertTrue(ValidationUtil.isNotBlank(responseObject.getItem().getHashId()));
+        Assertions.assertTrue(ValidationUtil.isNotBlank(responseObject.getItem().getAddress().getHashId()));
+    }
+
+    @DisplayName(value = "Teste de falha - Criar um outro usuário dono estando autenticado como um outro tipo de usuário")
+    @Test
+    public void createAnotherOwnerUserWithoutBeingAuthenticatedAsOwnerFailure() {
+        HttpHeaders headers = httpHeaderComponent.generateHeaderWithClientBearerToken();
+        UserPostRequestDTO userPostRequestDTO = JsonUtil.objectFromJson("userPostRequestDTOOwner", PATH_RESOURCE_USER, UserPostRequestDTO.class, DatePatternEnum.DATE_FORMAT_mm_dd_yyyy_WITH_SLASH.getValue());
+        ResponseEntity<?> responseEntity = testRestTemplate.exchange("/api/v1/users", HttpMethod.POST, new HttpEntity<>(userPostRequestDTO, headers), new ParameterizedTypeReference<>() {});
+        BaseErrorResponse403 responseObject = httpBodyComponent.responseEntityToObject(responseEntity, new TypeToken<>() {});
+        Assertions.assertEquals(HttpStatus.FORBIDDEN.value(), responseEntity.getStatusCode().value());
+        Assertions.assertEquals(HttpStatus.FORBIDDEN.value(), responseObject.getStatus());
+        Assertions.assertFalse(responseObject.isSuccess());
+        Assertions.assertTrue(ValidationUtil.isNotEmpty(responseObject.getMessages()));
+    }
+
+    @DisplayName(value = "Teste de falha - Criar um outro usuário dono não estando autenticado")
+    @Test
+    public void createOwnerUserWithoutBeingAuthenticatedFailure() {
+        HttpHeaders headers = httpHeaderComponent.generateHeaderWithoutBearerToken();
+        UserPostRequestDTO userPostRequestDTO = JsonUtil.objectFromJson("userPostRequestDTOOwner", PATH_RESOURCE_USER, UserPostRequestDTO.class, DatePatternEnum.DATE_FORMAT_mm_dd_yyyy_WITH_SLASH.getValue());
+        ResponseEntity<?> responseEntity = testRestTemplate.exchange("/api/v1/users", HttpMethod.POST, new HttpEntity<>(userPostRequestDTO, headers), new ParameterizedTypeReference<>() {});
+        BaseErrorResponse403 responseObject = httpBodyComponent.responseEntityToObject(responseEntity, new TypeToken<>() {});
+        Assertions.assertEquals(HttpStatus.FORBIDDEN.value(), responseEntity.getStatusCode().value());
+        Assertions.assertEquals(HttpStatus.FORBIDDEN.value(), responseObject.getStatus());
+        Assertions.assertFalse(responseObject.isSuccess());
+        Assertions.assertTrue(ValidationUtil.isNotEmpty(responseObject.getMessages()));
+    }
 }
