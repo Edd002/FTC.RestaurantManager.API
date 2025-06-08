2fb32692b75ed949e2ed24561bd4748ba344c5ae
Eduardo Augusto Lima Pereira <42621512+Edd002@users.noreply.github.com>
2025-06-08 20:30:03 -0300

    #Fase-2-estrutura-entidades-restaurante : Adição de estrutura de migrations de novas entidades para o contexto de testes automatizados

---
+++ src/test/resources/db/migration/h2/V2025.06.08_1__Create_t_menu.sql
@@ -0,0 +1,16 @@
+CREATE TABLE public.t_menu (
+    id int8 NOT NULL,
+    created_by varchar(255) NULL,
+    created_in timestamp(6) NOT NULL,
+    deleted bool NOT NULL,
+    deleted_by varchar(255) NULL,
+    deleted_in timestamp(6) NULL,
+    hash_id varchar(255) NOT NULL,
+    updated_by varchar(255) NULL,
+    updated_in timestamp(6) NULL,
+    primary key (id)
+);
+
+CREATE SEQUENCE public.sq_menu START WITH 1 INCREMENT BY 1;
+
+CREATE UNIQUE INDEX T_MENU_HASH_ID_UK ON public.t_menu (hash_id);
---
+++ src/test/resources/db/migration/h2/V2025.06.08_2__Create_t_menu_item.sql
@@ -0,0 +1,24 @@
+CREATE TABLE public.t_menu_item (
+    id int8 NOT NULL,
+    created_by varchar(255) NULL,
+    created_in timestamp(6) NOT NULL,
+    deleted bool NOT NULL,
+    deleted_by varchar(255) NULL,
+    deleted_in timestamp(6) NULL,
+    hash_id varchar(255) NOT NULL,
+    updated_by varchar(255) NULL,
+    updated_in timestamp(6) NULL,
+    availability bool NOT NULL,
+    description varchar(255) NOT NULL,
+    name varchar(255) NOT NULL,
+    photo_url varchar(255) NOT NULL,
+    price numeric(7, 2) NOT NULL,
+    fk_menu int8 NOT NULL,
+    primary key (id)
+);
+
+CREATE SEQUENCE public.sq_menu_item START WITH 1 INCREMENT BY 1;
+
+ALTER TABLE public.t_menu_item ADD NAME_UK_FIELD VARCHAR(255) AS (CASE deleted WHEN TRUE THEN NULL ELSE name END);
+CREATE UNIQUE INDEX T_MENU_ITEM_HASH_ID_UK ON public.t_menu_item (hash_id);
+CREATE UNIQUE INDEX T_MENU_ITEM_NAME_UK ON public.t_menu_item (NAME_UK_FIELD);
