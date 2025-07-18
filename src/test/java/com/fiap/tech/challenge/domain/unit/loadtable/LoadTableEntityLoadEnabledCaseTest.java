1a4bda8f1af74a31a704e385eb664be5623b94e8
Marcos Berto <marcosberto66@gmail.com>
2025-07-17 22:34:10 -0300

    Inclusões de testes para as classes UseCase do LoadTable

--- src/test/java/com/fiap/tech/challenge/domain/unit/loadtable/LoadTableEntityLoadEnabledCaseTest.java
+++ src/test/java/com/fiap/tech/challenge/domain/unit/loadtable/LoadTableEntityLoadEnabledCaseTest.java
@@ -1,4 +1,29 @@
 package com.fiap.tech.challenge.domain.unit.loadtable;
 
-public class LoadTableEntityLoadEnabledCaseTest {
+import com.fiap.tech.challenge.domain.loadtable.entity.LoadTable;
+import com.fiap.tech.challenge.domain.loadtable.usecase.LoadTableEntityLoadEnabledCase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoadTableEntityLoadEnabledCaseTest {
+
+    @Test
+    @DisplayName("Teste de sucesso - Deve retornar true quando LoadTable for null")
+    void shouldReturnTrueWhenLoadTableIsNull() {
+        LoadTableEntityLoadEnabledCase useCase = new LoadTableEntityLoadEnabledCase(null);
+
+        assertTrue(useCase.isEntityLoadEnabled());
+    }
+
+    @Test
+    @DisplayName("Teste de sucesso - Deve retornar false quando LoadTable.entityLoadEnabled for false")
+    void shouldReturnFalseWhenLoadTableEntityLoadEnabledIsFalse() {
+        LoadTable loadTable = new LoadTable("Restaurant");
+
+        LoadTableEntityLoadEnabledCase useCase = new LoadTableEntityLoadEnabledCase(loadTable);
+
+        assertFalse(useCase.isEntityLoadEnabled());
+    }
 }
