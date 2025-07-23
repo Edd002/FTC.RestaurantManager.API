fb6cd881f89c93143814b1114e9769f1e8706bbe
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2025-07-23 20:56:04 -0300

    wip: adiciona testes unitários de falha para restaurant user

---
+++ src/test/java/com/fiap/tech/challenge/domain/factory/UserTestFactory.java
@@ -0,0 +1,32 @@
+package com.fiap.tech.challenge.domain.factory;
+
+import com.fiap.tech.challenge.domain.user.entity.User;
+import com.fiap.tech.challenge.domain.usertype.entity.UserType;
+
+public class UserTestFactory {
+
+    public static User loadEntityOwner() {
+        return new User(
+                "Manu",
+                "manu@email.com",
+                "manu",
+                "123456789012345678901234",
+                "123456",
+                new UserType("Owner"),
+                RestaurantTestFactory.loadDefaultAddress()
+        );
+    }
+
+    public static User loadEntityAdmin() {
+        return new User(
+                "Manu",
+                "manu@email.com",
+                "manu",
+                "123456789012345678901234",
+                "123456",
+                new UserType("Admin"),
+                RestaurantTestFactory.loadDefaultAddress()
+        );
+    }
+
+}
--- src/test/java/com/fiap/tech/challenge/domain/unit/restaurantuser/RestaurantUserCheckForDeleteUseCaseTest.java
+++ src/test/java/com/fiap/tech/challenge/domain/unit/restaurantuser/RestaurantUserCheckForDeleteUseCaseTest.java
@@ -1,4 +1,35 @@
 package com.fiap.tech.challenge.domain.unit.restaurantuser;
 
+import com.fiap.tech.challenge.domain.factory.RestaurantUserTestFactory;
+import com.fiap.tech.challenge.domain.factory.UserTestFactory;
+import com.fiap.tech.challenge.domain.restaurantuser.usecase.RestaurantUserCheckForDeleteUseCase;
+import com.fiap.tech.challenge.global.exception.EntityCannotBeDeletedException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class RestaurantUserCheckForDeleteUseCaseTest {
+
+    @Test
+    void shouldThrowEntityCannotBeDeletedExceptionIfUserIsOwner() {
+        EntityCannotBeDeletedException exception =  assertThrows(EntityCannotBeDeletedException.class, () -> new RestaurantUserCheckForDeleteUseCase(UserTestFactory.loadEntityOwner(), List.of(RestaurantUserTestFactory.loadEntityRestaurantUser())));
+
+        assertEquals("Não foi possível realizar a exclusão pois deve existir pelo menos uma associação de usuário dono de restaurante com o restaurante.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Teste de falha - Deve lançar NullPointerException se Usuário for nulo")
+    void shouldThrowExceptionIfUserIsNull() {
+        assertThrows(NullPointerException.class, () -> new RestaurantUserCheckForDeleteUseCase(null, List.of(RestaurantUserTestFactory.loadEntityRestaurantUser())));
+    }
+
+    @Test
+    @DisplayName("Teste de falha - Deve lançar NullPointerException se lista de RestaurantUser for nulo")
+    void shouldThrowExceptionIfRestaurantIsNull() {
+        assertThrows(NullPointerException.class, () -> new RestaurantUserCheckForDeleteUseCase(UserTestFactory.loadEntityOwner(), null));
+    }
 }
