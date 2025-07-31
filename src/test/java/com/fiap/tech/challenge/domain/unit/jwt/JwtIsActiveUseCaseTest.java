c23f62f5776347c00f23f346fef7cf924d8f48f0
Marcos Berto <marcosberto66@gmail.com>
2025-07-30 22:26:43 -0300

    Criação dos testes unitários para a JWT

--- src/test/java/com/fiap/tech/challenge/domain/unit/jwt/JwtIsActiveUseCaseTest.java
+++ src/test/java/com/fiap/tech/challenge/domain/unit/jwt/JwtIsActiveUseCaseTest.java
@@ -1,4 +1,52 @@
 package com.fiap.tech.challenge.domain.unit.jwt;
 
+import com.fiap.tech.challenge.domain.factory.JwtTestFactory;
+import com.fiap.tech.challenge.domain.jwt.entity.Jwt;
+import com.fiap.tech.challenge.domain.jwt.usecase.JwtIsActiveUseCase;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class JwtIsActiveUseCaseTest {
+
+    private JwtTestFactory jwtTestFactory;
+
+    @BeforeEach
+    void setUp() {
+        jwtTestFactory = new JwtTestFactory();
+    }
+
+    @Test
+    @DisplayName("Teste de sucesso - Deve retornar true se o JWT estiver ativo")
+    void shouldReturnTrueIfJwtIsActive() {
+        Jwt jwt = jwtTestFactory.loadDefaultJwt();
+
+        JwtIsActiveUseCase useCase = new JwtIsActiveUseCase(jwt, 300000);
+        boolean result = useCase.isActive();
+
+        assertTrue(result);
+    }
+
+    @Test
+    @DisplayName("Teste de falha - Deve retornar false se o JWT estiver expirado")
+    void shouldReturnFalseIfJwtIsExpired() {
+        Jwt jwt = jwtTestFactory.loadExpiredTokenJwt();
+
+        JwtIsActiveUseCase useCase = new JwtIsActiveUseCase(jwt, 1000);
+        boolean result = useCase.isActive();
+
+        assertFalse(result);
+    }
+
+    @Test
+    @DisplayName("Teste de falha - Deve retornar false se o JWT for nulo")
+    void shouldReturnFalseIfJwtIsNull() {
+        JwtIsActiveUseCase useCase = new JwtIsActiveUseCase(null, 1000);
+        boolean result = useCase.isActive();
+
+        assertFalse(result);
+    }
 }
